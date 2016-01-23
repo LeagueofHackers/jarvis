8e9cdef0b78ccabb00836242bbaf9fa76235d51d
Monish Gupta <monish.gupta1@gmail.com>
2016-01-23 22:37:52 +0530

    port for API moved to 5100

--- bot/CONSTANTS.js
+++ bot/CONSTANTS.js
@@ -1,6 +1,6 @@
 module.exports = {
 	LEAGUE_OF_HACKERS_BOT_TOKEN: process.env.LEAGUE_OF_HACKERS_BOT_TOKEN,
-	API_PORT_NO: process.env.PORT,
+	API_PORT_NO: 5100,
 	CLIENT_ID: process.env.CLIENT_ID,
 	CLIENT_SECRET: process.env.CLIENT_SECRET,
 	REDIRECT_URI: process.env.REDIRECT_URI
